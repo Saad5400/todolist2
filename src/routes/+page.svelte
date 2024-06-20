709f3752197801fcb1a4344182e8e7d86f5f3a05
Saad5400 <sdbtwa@gmail.com>
2024-06-20 19:02:49 +0300

    adding tasks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,33 @@
 <script lang="ts">
   import Header from "$lib/Header.svelte";
-  import { name } from "$lib/stores/name";
+  import { tasks } from "$lib/stores/tasks";
+  import dayjs from "dayjs";
 
-  name.update((name: string) => {
-    return name + "1";
-  });
+  let title = "";
 
-  console.log($name);
+  function addTask() {
+    tasks.update((currentTasks) => {
+      currentTasks.push({
+        title,
+        assignedDate: dayjs(),
+        isDone: false,
+      });
+
+      return currentTasks;
+    });
+    title = "";
+  }
 </script>
 
 <div class="p-16 flex flex-col gap-8">
   <Header />
   <div class="input-group input-group-divider flex justify-between">
-    <input class="flex-1 !bg-white" type="search" placeholder="عنوان المهمة" />
-    <button class="variant-filled-primary">إضافة</button>
+    <input
+      bind:value={title}
+      class="flex-1 !bg-white"
+      type="search"
+      placeholder="عنوان المهمة"
+    />
+    <button on:click={addTask} class="variant-filled-primary">إضافة</button>
   </div>
 </div>
