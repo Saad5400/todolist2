16a91c1df5ff841f473e38f6ef3cc05343fe0960
Saad5400 <sdbtwa@gmail.com>
2024-06-20 17:58:26 +0300

    input ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
   import Header from "$lib/Header.svelte";
 </script>
 
-<div class="p-16">
-  <Header /> 
+<div class="p-16 flex flex-col gap-8">
+  <Header />
+  <div class="input-group input-group-divider flex justify-between">
+    <input class="flex-1 !bg-white" type="search" placeholder="عنوان المهمة" />
+    <button class="variant-soft-primary">إضافة</button>
+  </div>
 </div>
