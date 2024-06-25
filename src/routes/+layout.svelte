9d37329b552cde9f27d90bd98ce7453ec0870094
Saad5400 <sdbtwa@gmail.com>
2024-06-25 03:41:36 +0300

    pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import "../app.postcss";
-
+  // @ts-ignore
+  import { pwaInfo } from "virtual:pwa-info";
   // Floating UI for Popups
   import {
     computePosition,
@@ -16,7 +17,13 @@
   import { initializeStores, Modal } from "@skeletonlabs/skeleton";
 
   initializeStores();
+
+  $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : "";
 </script>
+
+<svelte:head>
+  {@html webManifestLink}
+</svelte:head>
 
 <Modal buttonPositive="variant-filled-error" />
 <slot />
